254f21adc3bfd4fe5818d276ef66a408f1f489b1
Yaroslav-Korchak <yakorchak@mail.ru>
2024-01-14 20:29:55 +0300

    фвÐadded conftest and negative test

---
+++ test_search_google.py
@@ -0,0 +1,13 @@
+from selene import have, browser
+
+
+def test_search_1(browser_set_up):
+    browser.open("https://www.google.com/")
+    browser.element('[name="q"]').type('yashaka/selene').press_enter()
+    browser.element('[id="search"]').should(have.text('User-oriented Web UI browser tests in Python'))
+
+
+def test_search_2(browser_set_up):
+    browser.open("https://www.google.com/")
+    browser.element('[name="q"]').type('0435968092435#%$#$%^#$%^$#%^').press_enter()
+    browser.element('[id="result-stats"]').should(have.text('Результатов: примерно 0'))
